d178cd9553d57a39e7c0c73334436776c72bd367
nishyanntomy <nishyanntomy@gmail.com>
2023-12-07 13:57:06 -0500

    add styling

--- src/pages/pending-requests/ClientRequestCard.tsx
+++ src/pages/pending-requests/ClientRequestCard.tsx
@@ -17,21 +17,25 @@ const ClientRequestCard: React.FC<ClientRequestCardProps> = ({ request, onAccept
   const { clientEmail, requestDate } = request;
 
   return (
-    <div className="border-2 border-gray-300 p-4 rounded mb-4">
-      <p className="text-xl font-bold mb-2">{clientEmail}</p>
-      <p className="mb-2">Request Date: {new Date(requestDate).toLocaleString()}</p>
-      <button
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
-        onClick={() => onAccept(clientEmail)}
-      >
-        Accept
-      </button>
-      <button
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => onReject(clientEmail)}
-      >
-        Reject
-      </button>
+    <div className="border-2 border-gray-300 p-4 rounded mb-4 flex justify-between items-center shadow-md bg-white">
+      <div>
+        <p className="text-xl font-bold mb-2">{clientEmail}</p>
+        <p className="mb-2">Request Date: {new Date(requestDate).toLocaleString()}</p>
+      </div>
+      <div className="flex space-x-4">
+        <button
+          onClick={() => onAccept(clientEmail)}
+          className="bg-green-300 hover:bg-green-400 text-white py-2 px-4 rounded focus:outline-none transition duration-300"
+        >
+          Accept
+        </button>
+        <button
+          onClick={() => onReject(clientEmail)}
+          className="bg-red-300 hover:bg-red-400 text-white py-2 px-4 rounded focus:outline-none transition duration-300"
+        >
+          Reject
+        </button>
+      </div>
     </div>
   );
 };
